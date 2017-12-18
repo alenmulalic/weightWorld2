7a8614fe587db3b4466108a088fd7296915b98ac
outofcake <34632586+outofcake@users.noreply.github.com>
2017-12-17 20:48:47 -0600

    added basic nav bar and added basic login and lift form will changelater

--- html/login.js
+++ html/login.js
@@ -1,4 +1,18 @@
 function loginFunction() {
-  email  = document.getElementById('inputEmail').value;
-  password  = document.getElementById('inputPassword').value;
+  var email  = document.getElementById('inputEmail').value;
+  alert(email)
+  var password  = document.getElementById('inputPassword').value;
+  alert(password)
+}
+
+function workoutFunction() {
+  var workout = document.getElementById('workout').value;
+  alert(workout)
+  var sets = document.getElementById('sets').value;
+  alert(sets)
+  var reps = document.getElementById('reps').value;
+  alert(reps)
+  var weight = document.getElementById('weight').value;
+  alert(weight)
+
 }
