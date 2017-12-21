60a95d2ba09ecefc735b4bc3a32f93c2c70bcdd5
outofcake <34632586+outofcake@users.noreply.github.com>
2017-12-20 20:50:31 -0600

    Works like a website now yaaaaaah

--- webSiteScript.py
+++ webSiteScript.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("indexTest.html")
+    return render_template("homePage.html")
+
+@app.route('/login/')
+def login():
+    return render_template("login.html")
+
+@app.route('/workout/')
+def workout():
+    return render_template("liftForm.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
