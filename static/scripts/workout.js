c1289395c60a3f4ef856efd9a744d11981c4baa4
outofcake <34632586+outofcake@users.noreply.github.com>
2017-12-25 17:22:09 -0600

    liftForm and workout.js updates

--- static/scripts/workout.js
+++ static/scripts/workout.js
@@ -1,8 +1,12 @@
 
 window.onload=function(){
   var workout = document.getElementById('workoutSubmit');
+  var workoutForm =document.getElementById('workoutForm');
 
   workout.addEventListener('click',function(){
+      var cln = workoutForm.cloneNode(true);
+      document.getElementById('mainForm').appendChild(cln);
       console.log('button has been clicked');
+
     });
 }
