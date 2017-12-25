679381af5f7aeca47ae849cb2af431e7f02d965a
outofcake <34632586+outofcake@users.noreply.github.com>
2017-12-25 16:08:20 -0600

    firebase login and signup should be working modified code to avoid null issue still need more tests

--- static/scripts/login.js
+++ static/scripts/login.js
@@ -9,42 +9,83 @@
 		messagingSenderId: "360326042761"
 	  };
 	  firebase.initializeApp(config);
-	 
  }());
- // Get elements
- const txtEmail = document.getElementById('inputEmail');
- const txtPassword = document.getElementById('inputPassword');
- const btnLogin = document.getElementById('btnLogin');
- const btnSignUp = document.getElementById('btnSignUp');
- 
- // Add login event
- btnLogin.addEventListener('click', e=> {
-	 // Get email and password
-	 const email = txtEmail.value;
-	 const pass = txtPassword.value;
-	 const auth = firebase.auth();
-	 //Sign in 
-	 const promise = auth.signInWithEmailAndPassword(email, pass);
-	 promise.catch(e => console.log(e.message));
- });
- 
+
+
+
+window.onload=function(){
+  //variable intilization
+  const txtEmail = document.getElementById('newEmail');
+  const txtPassword = document.getElementById('newPassword');
+  const loginEmail =document.getElementById('inputEmail');
+  const loginPassword = document.getElementById('inputPassword');
+  const btnSignUp = document.getElementById('newSignUp');
+  const btnLogin = document.getElementById('newSignIn');
+
+
+
+  //signup
+  btnSignUp.addEventListener('click', e=>{
+    const email = txtEmail.value;
+ 	  const pass = txtPassword.value;
+ 	  const auth = firebase.auth();
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+
+  });
+
+  //signIn
+
+  btnLogin.addEventListener('click', e=> {
+    //get email and password
+    const email = loginEmail.value;
+    const pass = loginPassword.value;
+    const auth = firebase.auth();
+    //Sign in
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+  });
+
+  firebase.auth().onAuthStateChanged(firebaseUser => {
+    if(firebaseUser) {
+      console.log(firebaseUser);
+    } else {
+      console.log('not logged in');
+    }
+  });
+
+}
+
+
+
+ // // Add login event
+ // btnSignUp.addEventListener('click', e=> {
+	//  // Get email and password
+	//  const email = txtEmail.value;
+	//  const pass = txtPassword.value;
+	//  const auth = firebase.auth();
+	//  //Sign in
+	//  const promise = auth.signInWithEmailAndPassword(email, pass);
+	//  promise.catch(e => console.log(e.message));
+ // });
+
   // Add SignUp event
- btnSignUp.addEventListener('click', e=> {
-	 // TODO: CHECK FOR A REAL EMAIL
-	 // Get email and password
-	 const email = txtEmail.value;
-	 const pass = txtPassword.value;
-	 const auth = firebase.auth();
-	 //Sign in 
-	 const promise = auth.createUserWithEmailAndPassword(email, pass);
-	 promise.catch(e => console.log(e.message));
- });
- 
- // Add a realtime listener
- firebase.auth().onAuthStateChanged(firebaseUser => {
-	 if(firebaseUser) {
-		 console.log(firebaseUser);
-	 } else {
-		 console.log('not logged in');
-	 }
- });
+ // btnSignUp.addEventListener('click', e=> {
+	//  // TODO: CHECK FOR A REAL EMAIL
+	//  // Get email and password
+	//  const email = txtEmail.value;
+	//  const pass = txtPassword.value;
+	//  const auth = firebase.auth();
+	//  //Sign in
+	//  const promise = auth.createUserWithEmailAndPassword(email, pass);
+	//  promise.catch(e => console.log(e.message));
+ // });
+ //
+ // // Add a realtime listener
+ // firebase.auth().onAuthStateChanged(firebaseUser => {
+	//  if(firebaseUser) {
+	// 	 console.log(firebaseUser);
+	//  } else {
+	// 	 console.log('not logged in');
+	//  }
+ // });
--- static/scripts/workout.js
+++ static/scripts/workout.js
@@ -1,3 +1,3 @@
 document.getElementById('workoutSubmit').addEventListener('click',function(){
     console.log('button has been clicked');
-  }
+  };
