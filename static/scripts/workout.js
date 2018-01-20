ccea0cb7b2c8169ee39ece51c7465bb663181a6b
outofcake <34632586+outofcake@users.noreply.github.com>
2018-01-19 20:04:20 -0600

    Workout Form js and html updates

--- static/scripts/workout.js
+++ static/scripts/workout.js
@@ -1,15 +1,23 @@
-//
-// window.onload=function(){
-//   var workout = document.getElementById('workoutSubmit');
-//   document.getElementById('mainForm').style.display ="none";
-// 
-//   workout.addEventListener('click',function(){
-//       document.getElementById('mainForm').style.display ="inherit";
-//       console.log('button has been clicked');
-//
-//       // var cln = workoutForm.cloneNode(true);
-//       // document.getElementById('mainForm').appendChild(cln);
-//       // console.log('button has been clicked');
-//
-//     });
-// }
+
+window.onload=function(){
+  var workout = document.getElementById('workoutSubmit');
+  document.getElementById('mainForm').style.display ="none";
+  var workoutAdd = document.getElementById('workoutAdd');
+  var workoutForm = document.getElementById('workoutForm');
+
+  workout.addEventListener('click',function(){
+      document.getElementById('mainForm').style.display ="inherit";
+      console.log('button has been clicked');
+
+      // var cln = workoutForm.cloneNode(true);
+      // document.getElementById('mainForm').appendChild(cln);
+      // console.log('button has been clicked');
+
+    });
+    workoutAdd.addEventListener('click', function() {
+      var cln = workoutForm.cloneNode(true);
+      document.getElementById('mainForm').appendChild(cln);
+      console.log('button has been clicked');
+    });
+
+}
