d51464a3925ff50e023084ede64aaddc51207bba
nboris55 <nborisenko55@gmail.com>
2018-01-19 19:31:51 -0600

    Add files via upload
    fixed login and signup

--- static/scripts/login.js
+++ static/scripts/login.js
@@ -1,91 +1,44 @@
- (function() {
-	// Initialize Firebase
-	  var config = {
-		apiKey: "AIzaSyC4veNMKpc2XyEtQJxg_5ZGNwmfBAexu8c",
-		authDomain: "weightworld-67d35.firebaseapp.com",
-		databaseURL: "https://weightworld-67d35.firebaseio.com",
-		projectId: "weightworld-67d35",
-		storageBucket: "weightworld-67d35.appspot.com",
-		messagingSenderId: "360326042761"
-	  };
-	  firebase.initializeApp(config);
- }());
+(function() {
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyC4veNMKpc2XyEtQJxg_5ZGNwmfBAexu8c",
+    authDomain: "weightworld-67d35.firebaseapp.com",
+    databaseURL: "https://weightworld-67d35.firebaseio.com",
+    projectId: "weightworld-67d35",
+    storageBucket: "weightworld-67d35.appspot.com",
+    messagingSenderId: "360326042761"
+    };
+    
+    firebase.initializeApp(config);
+    //Get elements
+    const txtEmail = document.getElementById("Email");
+    const txtPassword = document.getElementById("Password");
+    const btnLogin = document.getElementById("btnLogin");
 
-
-
-window.onload=function(){
-  //variable intilization
-  const txtEmail = document.getElementById('newEmail');
-  const txtPassword = document.getElementById('newPassword');
-  const loginEmail =document.getElementById('inputEmail');
-  const loginPassword = document.getElementById('inputPassword');
-  const btnSignUp = document.getElementById('newSignUp');
-  const btnLogin = document.getElementById('newSignIn');
-
-
-
-  //signup
-  btnSignUp.addEventListener('click', e=>{
+    //add login event
+    btnLogin.addEventListener('click', e=> {
+    //get email and Password
     const email = txtEmail.value;
- 	  const pass = txtPassword.value;
- 	  const auth = firebase.auth();
-    const promise = auth.createUserWithEmailAndPassword(email, pass);
-    promise.catch(e => console.log(e.message));
-
-  });
-
-  //signIn
-
-  btnLogin.addEventListener('click', e=> {
-    //get email and password
-    const email = loginEmail.value;
-    const pass = loginPassword.value;
+    const pass = txtPassword.value;
     const auth = firebase.auth();
+
     //Sign in
-    const promise = auth.createUserWithEmailAndPassword(email, pass);
-    promise.catch(e => console.log(e.message));
-  });
-
-  firebase.auth().onAuthStateChanged(firebaseUser => {
-    if(firebaseUser) {
-      console.log(firebaseUser);
-    } else {
-      console.log('not logged in');
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.then(user => {
+      if (user.emailVerified) {
+      //Add a realtime listener
+      firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+      // User is signed in.
+      console.log("user signed in")
+      } else {
+      // No user is signed in.
+      console.log("no user signed in")
+      }
+      });
+      } else {
+      console.log("error");
     }
-  });
-
-}
-
-
-
- // // Add login event
- // btnSignUp.addEventListener('click', e=> {
-	//  // Get email and password
-	//  const email = txtEmail.value;
-	//  const pass = txtPassword.value;
-	//  const auth = firebase.auth();
-	//  //Sign in
-	//  const promise = auth.signInWithEmailAndPassword(email, pass);
-	//  promise.catch(e => console.log(e.message));
- // });
-
-  // Add SignUp event
- // btnSignUp.addEventListener('click', e=> {
-	//  // TODO: CHECK FOR A REAL EMAIL
-	//  // Get email and password
-	//  const email = txtEmail.value;
-	//  const pass = txtPassword.value;
-	//  const auth = firebase.auth();
-	//  //Sign in
-	//  const promise = auth.createUserWithEmailAndPassword(email, pass);
-	//  promise.catch(e => console.log(e.message));
- // });
- //
- // // Add a realtime listener
- // firebase.auth().onAuthStateChanged(firebaseUser => {
-	//  if(firebaseUser) {
-	// 	 console.log(firebaseUser);
-	//  } else {
-	// 	 console.log('not logged in');
-	//  }
- // });
+  })
+});
+}());
